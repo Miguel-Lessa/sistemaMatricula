67d68e87b2977b4e28b41814f79c595acd3b09a6
mikapoggerz <mikapoggers@gmail.com>
2025-02-17 17:53:30 -0300

    Métodos de matricula e cancelar matricula implementados

--- sistemaMatriculas/src/model/Aluno.java
+++ sistemaMatriculas/src/model/Aluno.java
@@ -4,13 +4,52 @@ import java.util.List;
 
 public class Aluno {
     private int matricula;
-    private List<Disciplina> disciplinasMatriculadas;
+    private List<Disciplina> disciplinasMatriculadasObrigatorias;
+    private List<Disciplina> disciplinasMatriculadasOptativas;
 
+
+    //Construtor
     public Aluno(int matricula, List<Disciplina> disciplinasMatriculadas) {
         this.matricula = matricula;
-        this.disciplinasMatriculadas = disciplinasMatriculadas;
+        this.disciplinasMatriculadasObrigatorias = disciplinasMatriculadasObrigatorias;
+        this.disciplinasMatriculadasOptativas = disciplinasMatriculadasOptativas;
     }
 
+
+    //Métodos
+
+    public void matricular(Disciplina disciplina) {
+        if (disciplina.isObrigatoria() == true){
+            if (disciplinasMatriculadasObrigatorias.size() <= 4){
+                disciplinasMatriculadasObrigatorias.add(disciplina);
+                System.out.println("Matricula na disciplina " + disciplina.getNome() + " realizada com sucesso!");
+            } else {
+                System.out.println("Numero máximo de disciplinas obrigatórias atingido");
+            }
+        } else if (disciplina.isObrigatoria() == false){
+            if (disciplinasMatriculadasOptativas.size() <= 2){
+                disciplinasMatriculadasOptativas.add(disciplina);
+                System.out.println("Matricula na disciplina "+ disciplina.getNome() +" realizada com sucesso!");
+            } else {
+                System.out.println("Numero máximo de disciplinas optativas atingido");
+            }
+        }
+    }
+
+    public void cancelarMatricula (Disciplina disciplina){
+        if (disciplinasMatriculadasObrigatorias.size() > 0 && disciplinasMatriculadasOptativas.size() > 0){
+            if (disciplina.isObrigatoria() == true){
+                disciplinasMatriculadasObrigatorias.remove(disciplina);
+                System.out.println("Disciplina obrigatória removida com sucesso!");
+            } else if (disciplina.isObrigatoria() == false){
+                disciplinasMatriculadasOptativas.remove(disciplina);
+                System.out.println("Disciplina optativa removida com sucesso!");
+            }
+        }
+    }
+
+
+    //Getter e Setter
     public int getMatricula() {
         return matricula;
     }
@@ -19,22 +58,19 @@ public class Aluno {
         this.matricula = matricula;
     }
 
-    public List<Disciplina> getDisciplinasMatriculadas() {
-        return disciplinasMatriculadas;
+    public List<Disciplina> getDisciplinasMatriculadasObrigatorias() {
+        return disciplinasMatriculadasObrigatorias;
     }
 
-    public void setDisciplinasMatriculadas(List<Disciplina> disciplinasMatriculadas) {
-        this.disciplinasMatriculadas = disciplinasMatriculadas;
+    public void setDisciplinasMatriculadasObrigatorias(List<Disciplina> disciplinasMatriculadasObrigatorias) {
+        this.disciplinasMatriculadasObrigatorias = disciplinasMatriculadasObrigatorias;
     }
 
-    public void matricular(Disciplina disciplina) {
-        System.out.println("Matricula realizada na disciplina " + disciplina.getNome());
-
+    public List<Disciplina> getDisciplinasMatriculadasOptativas() {
+        return disciplinasMatriculadasOptativas;
     }
 
-
-    public void cancelarMatricula (Disciplina disciplina){
-        System.out.println("Matricula cancelada na disciplina " + disciplina.getNome());
-
+    public void setDisciplinasMatriculadasOptativas(List<Disciplina> disciplinasMatriculadasOptativas) {
+        this.disciplinasMatriculadasOptativas = disciplinasMatriculadasOptativas;
     }
 }
--- sistemaMatriculas/src/model/Disciplina.java
+++ sistemaMatriculas/src/model/Disciplina.java
@@ -63,4 +63,12 @@ public class Disciplina {
     public List<Aluno> getAlunosMatriculados() {
         return alunosMatriculados;
     }
+
+    public boolean isObrigatoria() {
+        return obrigatoria;
+    }
+
+    public void setObrigatoria(boolean obrigatoria) {
+        this.obrigatoria = obrigatoria;
+    }
 }
