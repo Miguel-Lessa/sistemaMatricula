661d79816c9ed0a4b09d5f2b3b61699f9fc59af3
Giovanna Ferreira <gio-vannah@hotmail.com>
2025-02-17 15:15:28 -0300

    Adiciona constructor e getters e setters da classe Cobranca

--- sistemaMatriculas/src/Cobranca.java
+++ sistemaMatriculas/src/Cobranca.java
@@ -3,6 +3,18 @@ import java.util.List;
 public class Cobranca {
     public Double total;
 
+    public Cobranca(Double total) {
+        this.total = total;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
     public void gerarCobraca (Aluno aluno, List<Disciplina> disciplinas){
         for (Disciplina disciplina : disciplinas){
             total = total = total + disciplina.getValor();
