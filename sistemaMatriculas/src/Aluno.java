83e0d722fded0dea14f7e26f4c5aea261cb532d7
Giovanna Ferreira <gio-vannah@hotmail.com>
2025-02-17 15:14:38 -0300

    Adiciona constructor e getters e setters na classe Aluno

--- sistemaMatriculas/src/Aluno.java
+++ sistemaMatriculas/src/Aluno.java
@@ -4,6 +4,26 @@ public class Aluno {
     private int matricula;
     private List<Disciplina> disciplinasMatriculadas;
 
+    public Aluno(int matricula, List<Disciplina> disciplinasMatriculadas) {
+        this.matricula = matricula;
+        this.disciplinasMatriculadas = disciplinasMatriculadas;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public List<Disciplina> getDisciplinasMatriculadas() {
+        return disciplinasMatriculadas;
+    }
+
+    public void setDisciplinasMatriculadas(List<Disciplina> disciplinasMatriculadas) {
+        this.disciplinasMatriculadas = disciplinasMatriculadas;
+    }
 
     public void matricular(Disciplina disciplina) {
         System.out.println("Matricula realizada na disciplina " + disciplina.getNome());
