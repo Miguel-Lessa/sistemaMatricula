d814e34d889be67ddb4966b987ca6bbf027ab8bb
Giovanna Ferreira <gio-vannah@hotmail.com>
2025-02-27 20:52:58 -0300

    Checkbox estilizado
    mas ainda não sei pq ele (ou a label) move quando clica

--- sistemaMatriculas/frontend/src/Components/Matricula/Matricula.jsx
+++ sistemaMatriculas/frontend/src/Components/Matricula/Matricula.jsx
@@ -7,53 +7,22 @@ const Matricula = () => {
       <h1>Matrícula</h1>
       <form action="/action_page.php">
         <div className={styles.checkboxcontainer}>
-          <input
-            type="checkbox"
-            id="disciplina1"
-            name="disciplina1"
-            value="Psicanalise"
-          />
-          <label htmlFor="disciplina1">Psicanálise</label>
+          <label>
+            <input type="checkbox" className={styles.checkbox} />
+            <span className={styles.checkmark}></span>Cálculo III
+          </label>
         </div>
-
         <div className={styles.checkboxcontainer}>
-          <input
-            type="checkbox"
-            id="disciplina2"
-            name="disciplina2"
-            value="Calculo"
-          />
-          <label htmlFor="disciplina2">Cálculo III</label>
+          <label>
+            <input type="checkbox" className={styles.checkbox} />
+            <span className={styles.checkmark}></span>Psicanálise
+          </label>
         </div>
-
         <div className={styles.checkboxcontainer}>
-          <input
-            type="checkbox"
-            id="disciplina3"
-            name="disciplina3"
-            value="Anatomia"
-          />
-          <label htmlFor="disciplina3">Anatomia Humana</label>
-        </div>
-
-        <div className={styles.checkboxcontainer}>
-          <input
-            type="checkbox"
-            id="disciplina4"
-            name="disciplina4"
-            value="Analise"
-          />
-          <label htmlFor="disciplina4">Análise de Obras Clássicas</label>
-        </div>
-
-        <div className={styles.checkboxcontainer}>
-          <input
-            type="checkbox"
-            id="disciplina5"
-            name="disciplina5"
-            value="Fisica"
-          />
-          <label htmlFor="disciplina5">Física Quântica</label>
+          <label>
+            <input type="checkbox" className={styles.checkbox} />
+            <span className={styles.checkmark}></span>Física Quântica
+          </label>
         </div>
 
         <div className={styles.botoes}>
