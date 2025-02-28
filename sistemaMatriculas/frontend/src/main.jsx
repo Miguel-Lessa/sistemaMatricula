8c99380664cc04fa0884d11bdd7b29404de58c69
Giovanna Ferreira <gio-vannah@hotmail.com>
2025-02-28 20:05:13 -0300

    Update main.jsx

--- sistemaMatriculas/frontend/src/main.jsx
+++ sistemaMatriculas/frontend/src/main.jsx
@@ -5,6 +5,12 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <a href="/login">Tela de Login | </a>
+    <a href="/alunoprofile">Tela do Aluno | </a>
+    <a href="/professorprofile">Tela do Professor | </a>
+    <a href="/admprofile">Tela do Adm | </a>
+    <a href="/matricula">Tela de Matrícula</a>
+
     <App />
   </StrictMode>
 );
