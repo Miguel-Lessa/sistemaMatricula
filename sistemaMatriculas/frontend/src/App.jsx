98ff8943b027e9e61e003083bf2e5512bd6e1288
Giovanna Ferreira <gio-vannah@hotmail.com>
2025-02-28 00:17:05 -0300

    Create App.jsx

---
+++ sistemaMatriculas/frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import AlunoProfile from "./Components/Aluno/AlunoProfile";
+import ProfessorProfile from "./Components/Professor/ProfessorProfile";
+import Matricula from "./Components/Matricula/Matricula";
+import Header from "./Header";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>teste</h1>
+      <BrowserRouter>
+        <Login />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/alunoprofile" element={<AlunoProfile />} />
+          <Route path="/matricula" element={<Matricula />} />
+          <Route path="/professorprofile" element={<ProfessorProfile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
